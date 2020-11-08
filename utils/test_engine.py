acac8f40c6227a65bf35dadd53f93c73b38dd5c1
Yash Khandelwal <yash12khandelwal@gmail.com>
2020-11-08 18:37:23 +0000

    variable rename, cleaned code

--- utils/test_engine.py
+++ utils/test_engine.py
@@ -1,71 +1,80 @@
 import torch
 import numpy as np
 
-def test_engine(test_dataset, model, device, hyperparams, best_of_n = 1):
-	"""Evalutes test metrics. Assumes all test data is in one batch"""
+from collections import defaultdict
 
+def test_engine(test_dataset, model, device, hyperparams: dict, best_of_n: int = 1) -> dict:
+	"""[summary]
+
+	Arguments:
+		test_dataset -- Test Dataset
+		model -- PECNet object
+		device -- Device to use (GPU or CPU)
+		hyperparams {dict} -- Dictionary stores all the hyperparams that are used while training
+
+	Keyword Arguments:
+		best_of_n {int} -- [description] (default: {1})
+
+	Returns:
+		dict -- Dictionary of the test error
+	"""
 	model.eval()
 	assert best_of_n >= 1 and type(best_of_n) == int
+	error_dict = defaultdict(lambda: 0)
 
 	with torch.no_grad():
-		for _, (traj, mask, initial_pos) in enumerate(zip(test_dataset.trajectory_batches, test_dataset.mask_batches, test_dataset.initial_pos_batches)):
-			traj, mask, initial_pos = torch.DoubleTensor(traj).to(device), torch.DoubleTensor(mask).to(device), torch.DoubleTensor(initial_pos).to(device)
-			x = traj[:, :hyperparams["past_length"], :]
-			y = traj[:, hyperparams["past_length"]:, :]
-			y = y.cpu().numpy()
+		for i, (traj, mask, initial_pos) in enumerate(zip(test_dataset.trajectory_batches, test_dataset.mask_batches, test_dataset.initial_pos_batches)):
+			
+			traj = torch.DoubleTensor(traj).to(device)
+			mask = torch.DoubleTensor(mask).to(device)
+			initial_pos = torch.DoubleTensor(initial_pos).to(device)
 
-			# reshape the data
-			x = x.view(-1, x.shape[1]*x.shape[2])
-			x = x.to(device)
+			past_traj = traj[:, :hyperparams["past_length"], :]
+			future_traj = traj[:, hyperparams["past_length"]:, :]
+			future_traj = future_traj.cpu().numpy()
 
-			dest = y[:, -1, :]
-			all_l2_errors_dest = []
+			past_traj = past_traj.view(-1, past_traj.shape[1]*past_traj.shape[2]).to(device)
+			dest = future_traj[:, -1, :]
+
+			all_fde = []
 			all_guesses = []
 			for _ in range(best_of_n):
+				pred_dest = model.forward(past_traj, initial_pos, device=device)
+				pred_dest = pred_dest.cpu().numpy()
+				all_guesses.append(pred_dest)
 
-				dest_recon = model.forward(x, initial_pos, device=device)
-				dest_recon = dest_recon.cpu().numpy()
-				all_guesses.append(dest_recon)
+				fde = np.linalg.norm(pred_dest - dest, axis = 1)
+				all_fde.append(fde)
 
-				l2error_sample = np.linalg.norm(dest_recon - dest, axis = 1)
-				all_l2_errors_dest.append(l2error_sample)
-
-			all_l2_errors_dest = np.array(all_l2_errors_dest)
+			all_fde = np.array(all_fde)
 			all_guesses = np.array(all_guesses)
-			# average error
-			l2error_avg_dest = np.mean(all_l2_errors_dest)
 
+			# final displacement error
+			fde = np.mean(all_fde)
+			# minimum final displacement error
+			avg_min_fde = np.mean(np.min(all_fde, axis = 0))
+			
 			# choosing the best guess
-			indices = np.argmin(all_l2_errors_dest, axis = 0)
-
-			best_guess_dest = all_guesses[indices,np.arange(x.shape[0]),  :]
-
-			# taking the minimum error out of all guess
-			l2error_dest = np.mean(np.min(all_l2_errors_dest, axis = 0))
-
+			indices = np.argmin(all_fde, axis = 0)
+			# TODO didn't understood this
+			best_guess_dest = all_guesses[indices, np.arange(past_traj.shape[0]),  :]
 			best_guess_dest = torch.DoubleTensor(best_guess_dest).to(device)
 
 			# using the best guess for interpolation
-			interpolated_future = model.predict(x, best_guess_dest, mask, initial_pos)
+			interpolated_future = model.predict(past_traj, best_guess_dest, mask, initial_pos)
+
+			# average displacement error
 			interpolated_future = interpolated_future.cpu().numpy()
 			best_guess_dest = best_guess_dest.cpu().numpy()
-
-			# final overall prediction
 			predicted_future = np.concatenate((interpolated_future, best_guess_dest), axis = 1)
 			predicted_future = np.reshape(predicted_future, (-1, hyperparams["future_length"], 2))
-			
-			# ADE error
-			l2error_overall = np.mean(np.linalg.norm(y - predicted_future, axis = 2))
+			ade = np.mean(np.linalg.norm(future_traj - predicted_future, axis = 2))
 
-			l2error_overall /= hyperparams["data_scale"]
-			l2error_dest /= hyperparams["data_scale"]
-			l2error_avg_dest /= hyperparams["data_scale"]
+			error_dict["ade"] += ade /hyperparams["data_scale"]
+			error_dict["avg_min_fde"] += avg_min_fde / hyperparams["data_scale"]
+			error_dict["fde"] = fde / hyperparams["data_scale"]
 
-			print(f"Test time error in destination best: {l2error_dest} and mean: {l2error_avg_dest}")
-			print(f"Test time error overall (ADE) best: {l2error_overall}")
+	for key in error_dict:
+		error_dict[key] /= (i + 1)
 
-	return {
-		"l2error_overall": l2error_overall,
-		"l2error_dest": l2error_dest,
-		"l2error_avg_dest": l2error_avg_dest
-	}
+	return error_dict
